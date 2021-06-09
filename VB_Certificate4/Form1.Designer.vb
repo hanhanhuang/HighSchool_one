35cbc1ae84841c49c68c54f64fa0758450c7cc38
hanhanhuang <75626762+hanhanhuang@users.noreply.github.com>
2021-06-09 19:53:20 +0800

    Add files via upload

---
+++ VB_Certificate4/Form1.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(34, 53)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "姓名"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(86, 50)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 25)
+        Me.TextBox1.TabIndex = 1
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.TextBox4)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.TextBox3)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(504, 42)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(482, 166)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "應檢人資料"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(86, 121)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 25)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(34, 124)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 15)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "座號"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(363, 50)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 25)
+        Me.TextBox3.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(260, 53)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(97, 15)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "丙級測試編碼"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(363, 121)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 25)
+        Me.TextBox4.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(290, 124)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(67, 15)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "考試日期"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(504, 254)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowTemplate.Height = 27
+        Me.DataGridView1.Size = New System.Drawing.Size(482, 303)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1393, 582)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ VB_Certificate4/Form1.vb
@@ -0,0 +1,67 @@
+﻿Public Class Form1
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim dt As New DataTable
+        Dim n1, num, pp, bp, count, s As Integer
+        'n1 讀第一筆資料看玩家玩幾局 num 撲克牌編號1-51
+        'pp 玩家點數 bp 莊家點數
+        's 序號 count 計數發牌次數
+        s = 1
+        Dim player, banker, result As String
+        Dim suit(4) As String '花色
+        Dim n2 As Double '從第二筆開始的隨機數
+        Dim card As New ArrayList '已抽牌的陣列
+
+        Dim ba1() As Byte = {226, 153, 160}
+        Dim ba2() As Byte = {226, 153, 165}
+        Dim ba3() As Byte = {226, 153, 166}
+        Dim ba4() As Byte = {226, 153, 163}
+        suit(0) = System.Text.Encoding.UTF8.GetString(ba1) '黑桃
+        suit(1) = System.Text.Encoding.UTF8.GetString(ba2)
+        suit(2) = System.Text.Encoding.UTF8.GetString(ba3)
+        suit(3) = System.Text.Encoding.UTF8.GetString(ba4)
+
+        dt.Columns.Add("序號")
+        dt.Columns.Add("玩家")
+        dt.Columns.Add("莊家")
+        dt.Columns.Add("結果")
+        FileOpen(1, "C:\丙檢測試資料\1060307.SM", OpenMode.Input)
+        Input(1, n1) '讀第一筆資料看要玩幾局
+
+        Do Until EOF(1)
+            Input(1, n2)
+            num = Int(n2 * 52) '算出隨機數*52且捨棄小數的值
+            If card.IndexOf(num) = -1 Then '尋找已抽牌陣列有沒有一樣的牌
+                '都沒有 回傳-1
+                card.Add(num) '加入已抽牌陣列
+                count = count + 1
+                If count = n1 * 2 Then Exit Do '當發牌次數=局數*2 結束發牌
+            End If
+        Loop
+        FileClose(1)
+
+        For i = 0 To n1 * 2 - 1 Step 2 '陣列從0開始計數 i從0開始 而每一局發兩張牌所以step2
+            Dim disp = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"} '題目要求0-12 為A-K
+            pp = card(i) Mod 13
+            bp = card(i + 1) Mod 13
+            player = suit(card(i) \ 13) & disp(pp) '\13 取整數找出花色,再搭配點數數値
+            banker = suit(card(i + 1) \ 13) & disp(bp)
+            If pp = 0 Then pp = 99
+            If bp = 0 Then bp = 99
+            If pp > bp Then result = "玩家贏"
+            If pp < bp Then result = "莊家贏"
+            If pp = bp Then result = "平手"
+
+            dt.Rows.Add(s, player, banker, result)
+            s += 1
+        Next
+        DataGridView1.DataSource = dt
+
+
+
+
+    End Sub
+End Class
