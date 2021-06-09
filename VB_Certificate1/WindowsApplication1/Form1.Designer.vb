54fe710fa2847d496aa55b376307199b02286c91
hanhanhuang <75626762+hanhanhuang@users.noreply.github.com>
2021-06-09 19:49:40 +0800

    Add files via upload

---
+++ VB_Certificate1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(96, 132)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(83, 39)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "第一題"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(96, 178)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(112, 215)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(249, 178)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(112, 215)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(249, 132)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(83, 39)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "第二題"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(389, 178)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(112, 215)
+        Me.TextBox3.TabIndex = 5
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(389, 132)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(83, 39)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "第三題"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(524, 178)
+        Me.TextBox4.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(112, 215)
+        Me.TextBox4.TabIndex = 7
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(524, 132)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(83, 39)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "第四題"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(660, 178)
+        Me.TextBox5.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(115, 210)
+        Me.TextBox5.TabIndex = 9
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(660, 132)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(83, 39)
+        Me.Button5.TabIndex = 8
+        Me.Button5.Text = "第五題"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(879, 31)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(98, 49)
+        Me.Button6.TabIndex = 10
+        Me.Button6.Text = "列印資料"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'PrintDocument1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1022, 514)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+End Class
---
+++ VB_Certificate1/WindowsApplication1/Form1.vb
@@ -0,0 +1,165 @@
+﻿Public Class Form1
+    '*******************************
+    '* 11900-1060301 Program Start *
+    '*******************************
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim m0, m1, m2, m3, m4 As Integer '宣告 m0,m1,m2,m3,m4 是整數格式
+        FileOpen(1, "C:\丙檢測試資料\1060301.SM", OpenMode.Input)
+        '設定為讀取模式
+        '設定成output檔案會被清空
+        Input(1, m0) '讀取檔案編號1的檔案中並將讀取資料存入m0
+        FileClose(1) '關閉一號檔案
+
+        m1 = m0
+        For i = 1 To 9
+            m2 = m1 \ 10
+            m3 = m1 Mod 10
+            m4 = m4 & m3
+            If m2 = 0 Then
+                Exit For
+            Else
+                m1 = m2
+            End If
+
+        Next
+
+
+
+        If m0 = m4 Then
+            TextBox1.Text = "第一題結果：" & m0 & " is a palidrome"
+        Else
+            TextBox1.Text = "第一題結果：" & m0 & " is not a palidrome"
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    '*******************************
+    '* 11900-1060302 Program Start *
+    '*******************************
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim m0 As Integer '宣告 m0 是整數格式
+        Dim s As String
+        FileOpen(1, "C:\丙檢測試資料\1060302.SM", OpenMode.Input)
+        Input(1, m0)
+        FileClose(1)
+
+        For j = 1 To m0
+            For i = 1 To j
+                s = s & i
+            Next
+            s = s & vbNewLine
+        Next
+        TextBox2.Text = "第二題結果：" & vbNewLine & s
+
+    End Sub
+    '*******************************
+    '* 11900-1060303 Program Start *
+    '*******************************
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim m0, m1, m2 As Integer
+
+        FileOpen(1, "C:\丙檢測試資料\1060303.SM", OpenMode.Input)
+        Input(1, m0)
+        FileClose(1)
+
+        m1 = m0
+
+        For j = 1 To m1
+            If m1 Mod j = 0 Then
+                m2 = m2 + 1
+            End If
+        Next
+        If m2 = 2 Then
+            TextBox3.Text = "第三題結果：" & m1 & " is a prime number"
+        Else
+            TextBox3.Text = "第三題結果：" & m1 & " is not a prime number"
+        End If
+
+
+    End Sub
+    '*******************************
+    '* 11900-1060304 Program Start *
+    '*******************************
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim h(3), w(3), bmi(3), min As Integer
+
+        FileOpen(1, "C:\丙檢測試資料\1060304.SM", OpenMode.Input)
+
+        For i = 1 To 3
+            Input(1, h(i)) : Input(1, w(i))
+            bmi(i) = w(i) / (h(i) / 100) ^ 2
+        Next
+        FileClose(1)
+        min = bmi(1)
+        For i = 1 To 3
+            If bmi(i) < min Then
+                min = bmi(i)
+            End If
+        Next
+
+        Dim min45 As Integer = min
+
+        If min45 < 20 Or min > 25 Then
+            TextBox4.Text = "第四題結果：BMI最小值為=" & min45 & "，不正常"
+        Else
+            TextBox4.Text = "第四題結果：BMI最小值為=" & min45 & "，正常"
+        End If
+
+    End Sub
+    '*******************************
+    '* 11900-1060305 Program Start *
+    '*******************************
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim a(2, 2), b(2, 2), c(2, 2) As Integer
+        FileOpen(1, "C:\丙檢測試資料\1060305.SM", OpenMode.Input)
+        Input(1, a(1, 1)) : Input(1, a(1, 2))
+        Input(1, a(2, 1)) : Input(1, a(2, 2))
+        Input(1, b(1, 1)) : Input(1, b(1, 2))
+        Input(1, b(2, 1)) : Input(1, b(2, 2))
+        FileClose(1)
+
+        For i = 1 To 2
+            For j = 1 To 2
+                c(i, j) = a(i, j) + b(i, j)
+            Next
+        Next
+
+        'do while loop
+        'Dim i = 1
+        'Do While i <= 2
+        '    Dim j = 1
+        '    Do While j <= 2
+        '        c(i, j) = a(i, j) + b(i, j)
+        '    Loop
+        'Loop
+        Dim m1 As String
+        m1 = m1 & "[" & c(1, 1) & "        " & c(1, 2) & "]" & vbNewLine
+        m1 = m1 & "[" & c(2, 1) & "     " & c(2, 2) & "]"
+
+        TextBox5.Text = "第五題結果：" & vbNewLine & m1
+
+    End Sub
+
+    Private Sub PrintDocument1_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
+        Dim data As String
+        Dim vn = vbNewLine
+        data = data & "姓名：[黃韋翰]     術科測試編號：[000000000]" & vn
+        data = data & "座號：[29]              日期[2019/06/04]" & vn & vn
+
+        data = data & TextBox1.Text & vn & vn
+        data = data & TextBox2.Text & vn & vn
+        data = data & TextBox3.Text & vn & vn
+        data = data & TextBox4.Text & vn & vn
+        data = data & TextBox5.Text & vn & vn
+
+        e.Graphics.DrawString(data, Me.Font, Brushes.Black, 100, 100)
+        'e 是指printDocument1 這個物件; Graphics-畫圖; Drawstring-畫出字串
+        '(要印的資料,字型,顏色,列印起始坐標)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Call PrintDocument1.Print()
+    End Sub
+End Class
